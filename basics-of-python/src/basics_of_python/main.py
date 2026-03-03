9f0f971e2326745b67a64f482573cd4ff9c54176
SURIYAPRABAKARAN <suriyaprabakara25@gmail.com>
2026-03-03 08:33:01 +0530

    variables adn def

--- basics-of-python/src/basics_of_python/main.py
+++ basics-of-python/src/basics_of_python/main.py
@@ -1,5 +1,27 @@
 def main():
     print("Hello I'm suriya here")
+    name = "suriya"
+    age = 10
+    print("name==> "+name)
+    print(type(name))
+    print(type(age))
+    a,b= 10,25
+    a,b=b,a
+    print(a , b)
+    result =   add(a,b)
+    print("result==> ",result)
+    if(result == 5):
+        print('that was me')
+    else:
+        print('not me')
+        greet()
+
+def add(a,b):
+    return a+b
+
+def greet(name='Guest'):
+    print("Hey ",name)
+
 
 if __name__ == "__main__":
     main()   
