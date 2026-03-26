2b6682961533fce8ef48624011975cb4794ddf21
SURIYAPRABAKARAN <suriyaprabakara25@gmail.com>
2026-03-26 08:08:42 +0530

    student result

---
+++ Mini Ml Projects/Student Result Predictor/StudentResultPredictor.py
@@ -0,0 +1,11 @@
+# for data handling making data frame
+import pandas as pd
+# for working with array
+import numpy as np
+
+from sklearn.tree import DecisionTreeClassifier
+
+
+df =  pd.read_csv("Mini Ml Projects/Employee Salary Predictor/StudentMarks.csv")
+
+print(df)
