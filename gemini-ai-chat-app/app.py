687a5d03f55d94897879b9f7dc036105ce61c7c3
SURIYAPRABAKARAN <suriyaprabakara25@gmail.com>
2026-05-31 15:28:29 +0530

    ai chat app

---
+++ gemini-ai-chat-app/app.py
@@ -0,0 +1,13 @@
+from google import genai
+
+client = genai.Client(api_key="YOUR_API_KEY")
+
+question = input("Ask something: ")
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents=question
+)
+
+print("\nAI Response:")
+print(response.text)
