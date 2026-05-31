92c1cac46970f943970afdcd4fecdd38e66cd09c
SURIYAPRABAKARAN <suriyaprabakara25@gmail.com>
2026-05-31 15:30:43 +0530

    added

--- gemini-ai-chat-app/app.py
+++ gemini-ai-chat-app/app.py
@@ -3,7 +3,7 @@ from google import genai
 client = genai.Client(api_key="YOUR_API_KEY")
 
 question = input("Ask something: ")
-//
+
 response = client.models.generate_content(
     model="gemini-2.5-flash",
     contents=question
