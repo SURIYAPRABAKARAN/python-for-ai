212e90cdf342395011dba58ab97eaae0ba0447c9
SURIYAPRABAKARAN <suriyaprabakara25@gmail.com>
2026-06-24 21:31:25 +0530

    lanchin rag completed

--- langchain-rag-chatbot/chat.py
+++ langchain-rag-chatbot/chat.py
@@ -1,18 +1,65 @@
-from ingest import retriever, llm, prompt
+from dotenv import load_dotenv
+from langchain_google_genai import (
+    GoogleGenerativeAIEmbeddings,
+    ChatGoogleGenerativeAI
+)
+from langchain_chroma import Chroma
+from langchain_core.prompts import PromptTemplate
 
-question = input("Ask: ")
+load_dotenv()
 
-docs = retriever.invoke(question)
-
-context = "\n\n".join(
-    [doc.page_content for doc in docs]
+embeddings = GoogleGenerativeAIEmbeddings(
+    model="gemini-embedding-2"
 )
 
-final_prompt = prompt.format(
-    context=context,
-    question=question
+vectorstore = Chroma(
+    persist_directory="chroma_db",
+    embedding_function=embeddings
 )
 
-response = llm.invoke(final_prompt)
+retriever = vectorstore.as_retriever(
+    search_kwargs={"k": 3}
+)
 
-print(response.content)
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0
+)
+
+prompt = PromptTemplate(
+    input_variables=["context", "question"],
+    template="""
+Answer ONLY from the provided context.
+
+Context:
+{context}
+
+Question:
+{question}
+
+Answer:
+"""
+)
+
+while True:
+
+    question = input("\nAsk: ")
+
+    if question.lower() == "exit":
+        break
+
+    docs = retriever.invoke(question)
+
+    context = "\n\n".join(
+        [doc.page_content for doc in docs]
+    )
+
+    final_prompt = prompt.format(
+        context=context,
+        question=question
+    )
+
+    response = llm.invoke(final_prompt)
+
+    print("\nAnswer:")
+    print(response.content)
--- langchain-rag-chatbot/ingest.py
+++ langchain-rag-chatbot/ingest.py
@@ -3,66 +3,27 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_chroma import Chroma
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.prompts import PromptTemplate
 
 load_dotenv()
 
-# pdf load
-pdf_path = "data/AI_Engineer_Roadmap.pdf"
-
-loader = PyPDFLoader(pdf_path)
-
+loader = PyPDFLoader("data/AI_Engineer_Roadmap.pdf")
 documents = loader.load()
 
-# print(documents[0].page_content[:300])
-
-# chunking
 splitter = RecursiveCharacterTextSplitter(
-    chunk_size=100,
-    chunk_overlap=30
+    chunk_size=500,
+    chunk_overlap=100
 )
 
 chunks = splitter.split_documents(documents)
 
-# embeddings
 embeddings = GoogleGenerativeAIEmbeddings(
     model="gemini-embedding-2"
 )
 
-# chroma db ( vector DB )
-vectorstore = Chroma.from_documents(
+Chroma.from_documents(
     documents=chunks,
     embedding=embeddings,
     persist_directory="chroma_db"
 )
 
-# retriving from chroma db we need top 3 mathcs so k = 3
-retriever = vectorstore.as_retriever(
-    search_kwargs={"k": 3}
-)
-
-
-# ai model for response maker
-llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
-    temperature=0
-)
-
-
-prompt = PromptTemplate(
-    input_variables=["context", "question"],
-    template="""
-You are a helpful assistant.
-
-Answer ONLY from the provided context.
-
-Context:
-{context}
-
-Question:
-{question}
-
-Answer:
-"""
-)
+print("Vector DB Created Successfully")
